b84b8558ab6dae7f3494ac1b08542bca142c3115
Gaurav Bhasin <bhasingaurav@gmail.com>
2021-06-08 18:57:26 -0400

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Gaurav Bhasin on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
